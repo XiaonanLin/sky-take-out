548090d8d676f655e9df9bdd2fd0e67e8718aa54
XiaonanLin <2680697157@qq.com>
2025-10-22 00:57:22 +0800

    feat: 商品浏览

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -54,6 +54,6 @@ public class UserController {
                 .token(token)
                 .build();
 
-        return Result.success(userLoginVo);
+             return Result.success(userLoginVo);
     }
 }
